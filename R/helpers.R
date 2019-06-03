f943a524271595741fee80d9706b7f010ded5da4
Danny Beachnau <dbeachnau@procarepain.com>
2019-06-03 09:31:53 -0400

    Add ifnull function

--- R/helpers.R
+++ R/helpers.R
@@ -4,3 +4,13 @@
 #'@export
 
 '%not_in%' <- function(x,y){!('%in%'(x,y))}
+
+
+#' Return default value if expression is null
+#'
+#' Mimics the us of ISNULL in SQL.
+#'
+#' @export
+ifnull <- function(expr, default){
+  ifelse(is.null(expr), default, expr)
+}
